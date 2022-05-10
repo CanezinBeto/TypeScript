d4640a5923d3656713bfe41296e34f5056a73bf0
CanezinBeto <albertocanezin@gmail.com>
2022-05-10 15:57:32 -0300

    Primeiro desafio concluido

---
+++ desafio-01/index.js
@@ -0,0 +1,5 @@
+// Função que recebe dois argumentos do tipo number
+function sum(number_01, number_02) {
+    return "A soma do n\u00FAmero ".concat(number_01, " e ").concat(number_02, " \u00E9 ").concat(number_01 + number_02);
+}
+console.log(sum(4, 1));
---
+++ desafio-01/index.ts
@@ -0,0 +1,8 @@
+// Função que recebe dois argumentos do tipo number
+function sum(number_01: number, number_02: number) {
+  return `A soma do número ${number_01} e ${number_02} é ${
+    number_01 + number_02
+  }`
+}
+
+console.log(sum(4, 1))
