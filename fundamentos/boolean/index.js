7ed68756eeed0d0f624c014c7cd31e7452bf1277
CanezinBeto <albertocanezin@gmail.com>
2022-05-11 09:36:37 -0300

    Fundamentos boolean

---
+++ fundamentos/boolean/index.js
@@ -0,0 +1,6 @@
+// Declando uma variável ou contante do tipo boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
---
+++ fundamentos/boolean/index.ts
@@ -0,0 +1,8 @@
+// Declando uma variável ou contante do tipo boolean
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
+
+a = true
+console.log(a)
