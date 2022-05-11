225fa050af8a4bbe49d1e37caaff4e00397a216c
CanezinBeto <albertocanezin@gmail.com>
2022-05-11 09:00:10 -0300

    Fundamentos number

---
+++ fundamentos/number/index.js
@@ -0,0 +1,8 @@
+// Tipando uma variável so tipo number
+var x = 10;
+console.log(x);
+x = 15;
+console.log(typeof x);
+// Ao tipar uma variável ou contante, ela só vai aceitar métodos do tipo passado à ela.
+var y = 3.1415;
+console.log(y.toPrecision(3));
---
+++ fundamentos/number/index.ts
@@ -0,0 +1,10 @@
+// Tipando uma variável so tipo number
+let x: number = 10
+console.log(x)
+
+x = 15
+console.log(typeof x)
+
+// Ao tipar uma variável ou contante, ela só vai aceitar métodos do tipo passado à ela.
+const y: number = 3.1415
+console.log(y.toPrecision(3))
