6c3e5c854ad5a4458a6320ddf3534fa8955c3438
CanezinBeto <albertocanezin@gmail.com>
2022-05-11 11:06:17 -0300

    Desafio 02 concluido, gerado arquivo de configuração do typescript

---
+++ desafio-02/index.js
@@ -0,0 +1,6 @@
+// Crie uma variável que recebe um número
+var number = 10;
+// Converta o número para string em uma nova variável, tipada por inferencia
+var numberString = number.toString();
+// Imprimir o número
+console.log("O n\u00FAmero ".concat(number, " foi transformado em string ").concat(numberString));
---
+++ desafio-02/index.ts
@@ -0,0 +1,8 @@
+// Crie uma variável que recebe um número
+const number: number = 10
+
+// Converta o número para string em uma nova variável, tipada por inferencia
+const numberString = number.toString()
+
+// Imprimir o número
+console.log(`O número ${number} foi transformado em string ${numberString}`)
