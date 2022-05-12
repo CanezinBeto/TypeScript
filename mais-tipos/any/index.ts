c04b5745d7cb42abdd56a4323807a036d4533cfa
CanezinBeto <albertocanezin@gmail.com>
2022-05-12 10:20:49 -0300

    Tipo Any

---
+++ mais-tipos/any/index.ts
@@ -0,0 +1,5 @@
+// Tipo any é quando não declaramos o tipo da variável, ou seja ela opde ser qualquer coisa
+// Não devemos usar esse tipo no typescript apesar de ele existir.
+
+// Tipo any
+const array: any = [1, 'dois', true, [], { nome: 'Alberto' }]
