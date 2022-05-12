9878b49ceba2b0b6ce8fcec53d8b128b3e4dc5fc
CanezinBeto <albertocanezin@gmail.com>
2022-05-12 15:09:16 -0300

    Avançando em union types

--- mais-tipos/union-type/index.ts
+++ mais-tipos/union-type/index.ts
@@ -5,3 +5,15 @@ function showBalance(balance: string | number) {
 
 showBalance(10000.0)
 showBalance('10000.00')
+
+// Avançando em union types
+// Dependendo do tipo de entrada em um parâmetro podemos retornar coisas diferentes.
+function showUserRole(role: boolean | string) {
+  if (typeof role === 'boolean') {
+    return `Este usuário é proibido`
+  }
+
+  return `Este usuário é ${role}`
+}
+
+console.log(showUserRole('Admin'), showUserRole(false))
