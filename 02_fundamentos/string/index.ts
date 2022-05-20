6581fc8f86f83e8ba385c0d983b9698b183aef10
CanezinBeto <albertocanezin@gmail.com>
2022-05-20 09:22:43 -0300

    Criando tipos, revisão generics

--- 02_fundamentos/string/index.ts
+++ 02_fundamentos/string/index.ts
@@ -1,16 +1,16 @@
 // Tipando uma variável ou constante para o tipo string
-const firstName: string = 'Alberto'
+const firstName01: string = 'Alberto'
 
 // Ao tipar uma variável pelo tipo string, ela só aceita métodos do tipo string
-console.log(firstName.toUpperCase())
+console.log(firstName01.toUpperCase())
 
 // Definir uma variável com seu tipo string, porém não atribur no momento da criação.
 let fullName: string
 
-const lastName: string = 'Canezin'
+const lastName01: string = 'Canezin'
 
 // Juntando as variáveis string
-fullName = `${firstName} ${lastName}`
+fullName = `${firstName01} ${lastName01}`
 
 console.log(fullName)
 console.log(typeof fullName)
