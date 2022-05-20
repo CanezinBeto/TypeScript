ac26edbc85922a2d25ea0e136133c98a5e6afd03
CanezinBeto <albertocanezin@gmail.com>
2022-05-20 08:50:18 -0300

    Tupla

---
+++ object-types/tuplas/index.ts
@@ -0,0 +1,8 @@
+// Tuplas é um tipo de array, porém definimos a quantidade e o tipo de elementos.
+type fiveNumbers = [number, number, number, number, number]
+
+const myNumberArray: fiveNumbers = [1, 2, 3, 4, 5]
+
+type nameAndAge = [string, number]
+
+const anotherUser: nameAndAge = ['Alberto', 32]
