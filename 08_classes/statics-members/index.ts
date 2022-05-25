f364dae56ca14c2a37baed28a8e6968450df50c1
CanezinBeto <albertocanezin@gmail.com>
2022-05-25 12:10:59 -0300

    Statics members

---
+++ 08_classes/statics-members/index.ts
@@ -0,0 +1,11 @@
+// Static members
+class StaticMembers {
+  static prop = 'Teste Static'
+
+  static method() {
+    console.log('Método static')
+  }
+}
+
+console.log(StaticMembers.prop)
+console.log(StaticMembers.method())
