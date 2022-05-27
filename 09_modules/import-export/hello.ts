ad67a640a1d29dc47aebd9710abe98d6e0e46443
CanezinBeto <albertocanezin@gmail.com>
2022-05-27 11:02:52 -0300

    Importação e exportação

---
+++ 09_modules/import-export/hello.ts
@@ -0,0 +1,7 @@
+// Exportando funções e variáveis
+export default function hello() {
+  console.log('Hello world')
+}
+
+// Exportando variáveis
+export const beto = 'Alberto'
---
+++ 09_modules/import-export/index.ts
@@ -0,0 +1,7 @@
+// Importando uma função
+import hello from './hello'
+// Importando variáveis
+import { beto } from './hello'
+
+hello()
+console.log(beto)
