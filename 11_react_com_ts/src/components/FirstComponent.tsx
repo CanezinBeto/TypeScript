f069e910440ab34bec9273cffdab209a654339de
CanezinBeto <albertocanezin@gmail.com>
2022-06-11 10:47:41 -0300

    Criando um componente

---
+++ 11_react_com_ts/src/components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+// 3 - Componentes ---- usando a flag tsrfce para criar um componente automático
+import React, { ReactElement } from 'react'
+
+export function FirstComponent(): ReactElement {
+  return (
+    <div>
+      <h2>Meu primeiro componente</h2>
+    </div>
+  )
+}
+
+export default FirstComponent
