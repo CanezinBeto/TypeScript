ca3fffc5f58989c277eb279e7ee9ec380a20d076
CanezinBeto <albertocanezin@gmail.com>
2022-06-11 09:57:50 -0300

    Variáveis com React

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,7 +1,17 @@
+import React from 'react'
+
+// Variáveis
+const name: string = 'Alberto'
+const age: number = 33
+const isWorking: boolean = true
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>Nome: {name}</p>
+      <p>Age: {age}</p>
+      {isWorking ? <p>Está trabalhando</p> : <p>Não está trabalhando</p>}
     </div>
   )
 }
