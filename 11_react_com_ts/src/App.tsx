80e6336021f5db3e0e0e5d2ae9cb754c9bab9d8a
CanezinBeto <albertocanezin@gmail.com>
2022-06-11 10:22:15 -0300

    Funções com react e typescript

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-// Variáveis
+// 1 - Variáveis
 const name: string = 'Alberto'
 const age: number = 33
 const isWorking: boolean = true
+
+// 2 - Funções
+const userGreeting = (name: string): string => {
+  return `Olá, ${name}`
+}
 
 function App() {
   return (
@@ -12,6 +17,8 @@ function App() {
       <p>Nome: {name}</p>
       <p>Age: {age}</p>
       {isWorking ? <p>Está trabalhando</p> : <p>Não está trabalhando</p>}
+
+      <h3>{userGreeting(name)}</h3>
     </div>
   )
 }
