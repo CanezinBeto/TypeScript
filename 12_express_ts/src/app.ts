6483efcd3a33a451e3b570a6fc3d23c4c0a51bfb
CanezinBeto <albertocanezin@gmail.com>
2022-06-17 20:18:09 -0300

    Iniciando o Express

--- 12_express_ts/src/app.ts
+++ 12_express_ts/src/app.ts
@@ -1,2 +1,14 @@
 // 1 - iniciando projeto
-console.log('Express + TypeScript!!!')
+// console.log('Express + TypeScript!!!')
+
+// 2 - Utilizando o Express
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Express</h1>')
+})
+
+app.listen(3000, () => {
+  console.log('Server Running')
+})
