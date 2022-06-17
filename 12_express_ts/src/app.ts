8651fe05c1bce1a469b9199a0ab77f15ea4ddfda
CanezinBeto <albertocanezin@gmail.com>
2022-06-17 20:32:27 -0300

    Rota com Post

--- 12_express_ts/src/app.ts
+++ 12_express_ts/src/app.ts
@@ -5,8 +5,17 @@
 import express from 'express'
 const app = express()
 
+// Habilitando JSON
+app.use(express.json())
+
+// 3 Rotas
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express</h1>')
+})
+
+app.post('/api/produtos', (req, res) => {
+  console.log(req.body)
+  return res.send('Produto Adicionado')
 })
 
 app.listen(3000, () => {
