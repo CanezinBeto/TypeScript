b17b3e65e52af99021083e51c177e2a3fd418862
CanezinBeto <albertocanezin@gmail.com>
2022-06-18 09:40:51 -0300

    Rota para qualquer verbo HTTP

--- 12_express_ts/src/app.ts
+++ 12_express_ts/src/app.ts
@@ -8,7 +8,7 @@ const app = express()
 // Habilitando JSON
 app.use(express.json())
 
-// 3 Rotas
+// 3 - Rotas
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express</h1>')
 })
@@ -16,6 +16,18 @@ app.get('/', (req, res) => {
 app.post('/api/produtos', (req, res) => {
   console.log(req.body)
   return res.send('Produto Adicionado')
+})
+
+// 4 - Rota para todos os verbos de requisição
+app.all('/api/produto/check', (req, res) => {
+  // req.method = VERBO HTTP
+  if (req.method === 'POST') {
+    return res.send('Inseriu algum resistro')
+  } else if (req.method === 'GET') {
+    return res.send('Leu algum registro')
+  } else {
+    return res.send('Não podemos realizar esta operação!')
+  }
 })
 
 app.listen(3000, () => {
